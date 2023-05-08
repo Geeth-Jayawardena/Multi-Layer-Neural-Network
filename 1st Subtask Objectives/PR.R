14eff99bf26912b54c9b7cf7f4b3dba938870739
Geeth-Jayawardena <101686253+Geeth-Jayawardena@users.noreply.github.com>
2023-05-08 22:00:54 +0530

    Rename column

--- 1st Subtask Objectives/PR.R
+++ 1st Subtask Objectives/PR.R
@@ -1,6 +1,10 @@
-#loading packages
+# loading packages
 library(readxl)
 
 # Import the data set
 dataset = read_xlsx('uow_consumption.xlsx')
 
+# Rename column
+colnames(dataset)[2] = "18th"
+colnames(dataset)[3] = "19th"
+colnames(dataset)[4] = "20th"
